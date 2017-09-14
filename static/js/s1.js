e4ab1afdcf505563cb03b139f7a40650a41f5a7b
miyamoto <miyamotok0105@gmail.com>
2017-09-14 12:36:23 +0900

    add

--- static/js/s1.js
+++ static/js/s1.js
@@ -1,6 +1,6 @@
 var margin = { top: 50, right: 0, bottom: 100, left: 100 },
           width = 980- margin.left - margin.right,
-          height = 430 - margin.top - margin.bottom,
+          height = 530 - margin.top - margin.bottom,
           gridSize = Math.floor(width / 24),
           legendElementWidth = gridSize*2,
           buckets = 6,
